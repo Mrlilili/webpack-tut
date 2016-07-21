dcdb175ffd327c485ea5dcf050f13383a1572095
“mrlilili” <ly1992@qq.com>
2016-07-21 11:08:40 +0800

    save

--- app/index.js
+++ app/index.js
@@ -1,16 +1,23 @@
+/**
+ * name: Webpack傻瓜式指南
+ * Reference link：https://zhuanlan.zhihu.com/p/20367175
+ * Reference link：https://zhuanlan.zhihu.com/p/20397902
+ */
 import './main.scss';
 import generateText from './sub';
 import $ from 'jquery';
+import 'imports?jQuery=jquery!./plugin.js'
 import moment from 'moment';
 
-let app  = document.createElement('div');
+let app = document.createElement('div');
 const myPromise = Promise.resolve(42);
 myPromise.then((number) => {
     $('body').append('<p>promise result is ' + number + ' now is ' + moment().format() + '</p>');
+    $('p').greenify()
 });
 app.innerHTML = '<h1>Hello World it</h1>';
 document.body.appendChild(app);
-app.appendChild(generateText());
+app.appendChild(generateText())
 
 
 
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,32 +4,43 @@ var HtmlwebpackPlugin = require('html-webpack-plugin');
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var TEM_PATH = path.resolve(ROOT_PATH, 'templates');
 var webpack = require('webpack');
 module.exports = {
     //项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
-    entry: APP_PATH,
+    entry: {
+        app: path.resolve(APP_PATH, 'index.js'),
+        mobile: path.resolve(APP_PATH, 'mobile.js'),
+        vendors: ['jquery', 'moment']
+    },
     //输出的文件名 合并以后的js会命名为bundle.js
     output: {
         path: BUILD_PATH,
-        filename: 'bundle.js'
+        filename: '[name].[hash].js'
     },
     //添加我们的插件 会自动生成一个html文件
     plugins: [
         new HtmlwebpackPlugin({
-            title: 'Hello World app'
+            title: 'Hello World app',
+            template: path.resolve(TEM_PATH, 'index.html'),
+            filename: 'index.html',
+            chunks: ['app', 'vendors'],
+            inject: 'body'
         }),
+        new HtmlwebpackPlugin({
+            title: 'Hello Mobile app',
+            template: path.resolve(TEM_PATH, 'mobile.html'),
+            filename: 'mobile.html',
+            chunks: ['mobile', 'vendors'],
+            inject: 'body'
+        }),
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
         //new webpack.ProvidePlugin({
         //    $: "jquery",
         //    jQuery: "jquery",
         //    "window.jQuery": "jquery"
         //})
     ],
-    devServer: {
-        historyApiFallBack: true,
-        hot: true,
-        inline: true,
-        progress: true,
-    },
     module: {
         perLoaders: [
             {
@@ -46,7 +57,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg)$/,
-                loader: 'url?limit'
+                loader: 'url?limit=2000'
             },
             {
                 test: /\.jsx?$/,
